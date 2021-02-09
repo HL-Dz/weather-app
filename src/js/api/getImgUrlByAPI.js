34c206e19b079fb088552ff95692077460d12e63
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-02-09 18:12:54 +0300

    feat: get backround image by API

---
+++ src/js/api/getImgUrlByAPI.js
@@ -0,0 +1,17 @@
+async function getBackgroundByAPI() {
+  const api = {
+    baseUrl: 'https://api.unsplash.com/photos/random',
+    token: 'i0m1BV9049JBkG9rVZhK3eiWdN2yT4twIQllKnc3hbo'
+  };
+
+  try {
+    let response = await fetch(`${api.baseUrl}?orientation=landscape&per_page=1&query=nature&client_id=${api.token}`)
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Something wrong: ${error}`);    
+  }
+  
+};
+
+export default getBackgroundByAPI;
